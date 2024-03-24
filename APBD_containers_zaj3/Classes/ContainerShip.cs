a147fa63c46b5d6fc80e68d7adb62ea34d43becc
MykolaKriuk <mykolakryukov@gmail.com>
2024-03-25 00:16:41 +0100

    Added some methods to ContainerShip class.

--- APBD_containers_zaj3/Classes/ContainerShip.cs
+++ APBD_containers_zaj3/Classes/ContainerShip.cs
@@ -1,11 +1,41 @@
 namespace APBD_containers_zaj3.Classes;
 
-public class ContainerShip(List<Container> containers, int maxSpeed, int MaxContainerValue, double MaxWeightToCarry)
+public class ContainerShip(int maxSpeed, int maxContainerValue, double maxWeightToCarry)
 {
-    public List<Container> Containers { get; set; } = containers;
+    private static int _idToAdd = 0;
+    public int Id { get; } = _idToAdd++;
+    public List<Container> Containers { get; set; } = [];
     public int MaxSpeed { get; set; } = maxSpeed;
-    public int MaxContainerValue { get; set; } = MaxContainerValue;
-    public double MaxWeightToCarry { get; set; } = MaxWeightToCarry;
-    
-    
+    public int MaxContainerValue { get; set; } = maxContainerValue;
+    public double MaxWeightToCarry { get; set; } = maxWeightToCarry;
+
+    private bool CheckTheAmountOfSpace(int amount)
+    {
+        return Containers.Count + amount < MaxContainerValue;
+    }
+    public void AddContainer(Container container)
+    {
+        if (CheckTheAmountOfSpace(1))
+            Containers.Add(container);
+        else
+            Console.WriteLine("Not enough space on the carrier.");
+    }
+
+    public void AddBundleOfContainers(List<Container> cons)
+    {
+        if (CheckTheAmountOfSpace(cons.Count))
+            foreach (var v in cons)
+                Containers.Add(v);
+        else
+            Console.WriteLine("Not enough space on the carrier.");
+    }
+
+    public void DeleteContainer(string id)
+    {
+        var conToRemove = Containers.FirstOrDefault(x => x.SerialNumber == id);
+        if (conToRemove == null) return;
+        Containers.Remove(conToRemove);
+        Console.WriteLine($"Container {id} is deleted.");
+
+    }
 }
